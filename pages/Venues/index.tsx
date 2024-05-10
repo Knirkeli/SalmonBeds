aee75464b190b03ee01c7cafc9b7f6a2a31b5ff0
Eirik Kirkeli <knirkeli88@msn.com>
2024-05-10 11:25:04 +0200

    Style and effects added to cards

--- pages/Venues/index.tsx
+++ pages/Venues/index.tsx
@@ -26,12 +26,12 @@ interface FetchData {
 
 const VenueCard: React.FC<{ venue: Venue }> = ({ venue }) => (
   <Link href={`/Venues/${venue.id}`} key={venue.id}>
-    <Card className="flex flex-col border border-gray-300 rounded p-4 mb-4 max-w-md m-auto">
+    <Card className="flex flex-col border border-gray-300 rounded p-4 mb-4 max-w-md m-auto h-auto transform transition duration-500 ease-in-out hover:shadow-lg hover:scale-105">
       <CardTitle className="text-lg text-center font-semibold mb-2">
         {venue.name}
       </CardTitle>
       {venue.media && venue.media.length > 0 && (
-        <div className="w-24 h-24 self-center overflow-hidden">
+        <div className="w-4/5 h-48 self-center overflow-hidden m-auto">
           <img
             className="w-full h-full object-cover"
             src={venue.media[0].url}
@@ -39,7 +39,7 @@ const VenueCard: React.FC<{ venue: Venue }> = ({ venue }) => (
           />
         </div>
       )}
-      <CardDescription className="text-base mb-2 mt-auto">
+      <CardDescription className="text-base text-center mb-2 mt-auto">
         Price: {venue.price ? venue.price : "N/A"} NOK/Night
       </CardDescription>
     </Card>
@@ -61,9 +61,13 @@ export default function Venues() {
     <>
       <Navbar />
       <h1 className="text-center text-4xl py-4">Venues</h1>
-      <div className="grid xs:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
-        {data &&
-          data.data.map((venue) => <VenueCard venue={venue} key={venue.id} />)}
+      <div className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="grid xs:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
+          {data &&
+            data.data.map((venue) => (
+              <VenueCard venue={venue} key={venue.id} />
+            ))}
+        </div>
       </div>
       <Footer />
     </>
