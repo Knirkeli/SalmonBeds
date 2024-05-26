d1ed3cac8919524d923b949727ff928bf6b53fad
Eirik Kirkeli <knirkeli88@msn.com>
2024-05-26 22:14:55 +0200

    refactor: Update page layout and metadata

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,12 @@
 // app/page.tsx
 import Venues from "./components/Venues";
 import RootLayout from "./layout";
-import Router from "next/router";
-import React, { useEffect, useState } from "react";
+import React from "react";
 
 export default function Home() {
   return (
     <RootLayout>
-      <Venues router={undefined} />
+      <Venues />
     </RootLayout>
   );
 }
